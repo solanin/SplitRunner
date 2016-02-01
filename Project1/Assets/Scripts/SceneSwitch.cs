f49decd48daf057bb3be45bd9daf0ab1da91929e
solanin <sjc5283@g.rit.edu>
2016-02-01 10:44:45 -0500

    Basic Menu & Unity Set up

---
+++ Project1/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour {
+
+	public string gotoName; // The name of the scene to load
+	
+	/// <summary>
+	/// Raises the mouse down event.
+	/// When clicked, loads level
+	/// </summary>
+	void OnMouseDown(){		
+		Application.LoadLevel(gotoName);
+	}
+}
