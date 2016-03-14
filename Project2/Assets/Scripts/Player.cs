d5e14714f3ac86d853b5c109d95dacf3322391d1
Conner Westover (RIT Student) <cqw1484@ad.rit.edu>
2016-03-14 14:35:12 -0400

    Initial Import of Project 2, Includes Player movement (WASD)

---
+++ Project2/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    GameObject self;
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        self = GameObject.FindGameObjectWithTag("Player");
+        rigidBody = self.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.velocity = new Vector3(-10.0f, rigidBody.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.velocity = new Vector3(10.0f, rigidBody.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 50.0f);
+        }
+    }
+}
