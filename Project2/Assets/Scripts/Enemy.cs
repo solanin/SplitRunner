074eb558e10309e576d9fe3f5ecde5347a05a7ea
Conner Westover (RIT Student) <cqw1484@ad.rit.edu>
2016-03-16 14:57:12 -0400

    Enemy Chases Player, Player Can Shoot Bullets, Jumping Height Changed for ease of manuvering around enemy

--- Project2/Assets/Scripts/Enemy.cs
+++ Project2/Assets/Scripts/Enemy.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+    GameObject player;
+    Rigidbody body;
+    public float speed = 25.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.FindGameObjectWithTag("Player");
+        body = this.GetComponentInParent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        body.AddForce((player.transform.position - this.transform.position).normalized * speed);
 	}
 }
--- Project2/Assets/Scripts/Player.cs
+++ Project2/Assets/Scripts/Player.cs
@@ -1,30 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     GameObject self;
     Rigidbody rigidBody;
+    bool isJumping;
+    int direction; // -1 left, 1 right
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         self = GameObject.FindGameObjectWithTag("Player");
         rigidBody = self.GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        direction = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             rigidBody.velocity = new Vector3(-10.0f, rigidBody.velocity.y);
+            direction = -1;
         }
         if (Input.GetKey(KeyCode.D))
         {
             rigidBody.velocity = new Vector3(10.0f, rigidBody.velocity.y);
+            direction = 1;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && !isJumping)
         {
-            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 50.0f);
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 60.0f);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            fireBullet();
+        }
+    }
+
+    void fireBullet()
+    {
+        GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        bullet.tag = "Bullet";
+        bullet.transform.localScale = new Vector3(.1f, .1f, .1f);
+        bullet.transform.position = this.transform.position + new Vector3(direction,0,0);
+        bullet.AddComponent<Rigidbody>();
+        bullet.GetComponent<Rigidbody>().velocity = new Vector3(30.0f * direction, 0.0f, 0.0f);
+        bullet.GetComponent<Rigidbody>().useGravity = false;
+        Destroy(bullet, 1.50f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isJumping = true;
         }
     }
 }
